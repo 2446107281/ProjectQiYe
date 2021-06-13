724cff808f6cb87ae8f4696a302ec03974584846
liuaojie <483806716@qq.com>
2021-06-13 10:28:34 +0800

    编译

--- GitProject/final/src/main/java/edu/ncst/controller/CustomerController.java
+++ GitProject/final/src/main/java/edu/ncst/controller/CustomerController.java
@@ -1,73 +1,71 @@
-package edu.ncst.controller;
-
-import edu.ncst.entity.Customer;
-import edu.ncst.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.servlet.http.HttpSession;
-import java.util.List;
-
-//controller三个功能 接受用户的输入，调用业务层完成相应的功能,返回页面相应数据
-@Controller
-@RequestMapping("/customer")
-public class CustomerController {
-    @Autowired
-    private CustomerService customerService;
-
-    @RequestMapping(value = "",method = RequestMethod.GET)
-    public String index(Model model){
-        List<Customer> customers = customerService.findAll();//调用业务层
-        model.addAttribute("customers",customers);//传递数据
-        return "customer_index";//返回网页
-    }
-    @RequestMapping(value = "/edit" ,method = RequestMethod.GET)
-    public  String edit(Integer id,Model model)
-    {
-        if (id == null){
-            Customer customer = new Customer();
-            model.addAttribute("entity",customer);
-        }
-        else{
-            Customer customer = customerService.findone(id);
-            model.addAttribute("entity",customer);
-        }
-        return "customer_edit";
-    }
-    @RequestMapping(value = "/edit" ,method = RequestMethod.POST)
-    public  String editSave(Customer customer,Model model)
-    {
-        customerService.save(customer);
-        List<Customer> customers = customerService.findAll();//调用业务层
-        model.addAttribute("customers",customers);//传递数据
-        return "customer_index";
-    }
-
-    @RequestMapping(value = "/delete",method = RequestMethod.GET)
-    public String delete(Integer id, Model model)
-    {
-        customerService.delete(id);
-        List<Customer> customers = customerService.findAll();//调用业务层
-        model.addAttribute("customers",customers);//传递数据
-        return "customer_index";
-    }
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String login()
-    {
-        return "login";
-    }
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(Customer customer, HttpSession session)
-    {
-        Customer c = customerService.login(customer.getUserName(),customer.getPassword());
-        if (c!=null){
-            session.setAttribute("customer",c);
-            return "product_index";
-        }
-        return "login";
-    }
-}
+//package edu.ncst.controller;
+//
+//import edu.ncst.service.CustomerService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//import javax.servlet.http.HttpSession;
+//import java.util.List;
+//
+////controller三个功能 接受用户的输入，调用业务层完成相应的功能,返回页面相应数据
+//@Controller
+//@RequestMapping("/customer")
+//public class CustomerController {
+//    @Autowired
+//    private CustomerService customerService;
+//
+//    @RequestMapping(value = "",method = RequestMethod.GET)
+//    public String index(Model model){
+//        List<Customer> customers = customerService.findAll();//调用业务层
+//        model.addAttribute("customers",customers);//传递数据
+//        return "customer_index";//返回网页
+//    }
+//    @RequestMapping(value = "/edit" ,method = RequestMethod.GET)
+//    public  String edit(Integer id,Model model)
+//    {
+//        if (id == null){
+//            Customer customer = new Customer();
+//            model.addAttribute("entity",customer);
+//        }
+//        else{
+//            Customer customer = customerService.findone(id);
+//            model.addAttribute("entity",customer);
+//        }
+//        return "customer_edit";
+//    }
+//    @RequestMapping(value = "/edit" ,method = RequestMethod.POST)
+//    public  String editSave(Customer customer,Model model)
+//    {
+//        customerService.save(customer);
+//        List<Customer> customers = customerService.findAll();//调用业务层
+//        model.addAttribute("customers",customers);//传递数据
+//        return "customer_index";
+//    }
+//
+//    @RequestMapping(value = "/delete",method = RequestMethod.GET)
+//    public String delete(Integer id, Model model)
+//    {
+//        customerService.delete(id);
+//        List<Customer> customers = customerService.findAll();//调用业务层
+//        model.addAttribute("customers",customers);//传递数据
+//        return "customer_index";
+//    }
+//    @RequestMapping(value = "/login",method = RequestMethod.GET)
+//    public String login()
+//    {
+//        return "login";
+//    }
+//    @RequestMapping(value = "/login",method = RequestMethod.POST)
+//    public String login(Customer customer, HttpSession session)
+//    {
+//        Customer c = customerService.login(customer.getUserName(),customer.getPassword());
+//        if (c!=null){
+//            session.setAttribute("customer",c);
+//            return "product_index";
+//        }
+//        return "login";
+//    }
+//}
--- GitProject/final/src/main/java/edu/ncst/controller/IngoodsController.java
+++ GitProject/final/src/main/java/edu/ncst/controller/IngoodsController.java
@@ -1,84 +1,83 @@
-package edu.ncst.controller;
-
-
-import edu.ncst.entity.Ingoods;
-import edu.ncst.service.IngoodsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
-@Controller
-@RequestMapping("/ingoods")
-public class IngoodsController {
-
-    @Autowired
-    private IngoodsService ingoodsService;
-
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model) {
-        List<Ingoods> ingoodss =ingoodsService.getAllIngoods();
-        model.addAttribute("ingoodss",ingoodss);
-        return "ingoods_index";
-    }
-
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add1(){ return "ingoods_update";}
-
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String add(Ingoods g, Model model){
-        ingoodsService.add(g);
-        List<Ingoods> ingoodss = ingoodsService.getAllIngoods();
-        model.addAttribute("ingoodss",ingoodss);
-        return "ingoods_index";
-    }
-    @RequestMapping(value = "/seek", method = RequestMethod.POST)
-    public String seek(Integer id, Model model){
-        Ingoods ingoods =ingoodsService.findone(id);
-        model.addAttribute("ingoods",ingoods);
-        return "ingoods_index";
-    }
-
-    @RequestMapping(value = "/update", method = RequestMethod.GET)
-    public String update1(Integer id,Model model){
-        Ingoods ingoods =ingoodsService.findone(id);
-        model.addAttribute("ingoods",ingoods);
-        return "ingoods_update";
-    }
-
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(Ingoods g, Model model){
-        ingoodsService.add(g);
-        List<Ingoods> ingoodss =ingoodsService.getAllIngoods();
-        model.addAttribute("ingoodss",ingoodss);
-        return "ingoods_index";
-    }
-
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Ingoods> list(){
-        return ingoodsService.getAllIngoods();
-    }
-
-    @RequestMapping(value = "/goods", method = RequestMethod.GET)
-    public String inGoods()
-    {
-        return "goods_index";
-    }
-
-    @RequestMapping(value = "/outgoods", method = RequestMethod.GET)
-    public String outGoods()
-    {
-        return "outgoods_index";
-    }
-
-    @RequestMapping(value = "/numgoods", method = RequestMethod.GET)
-    public String numGoods()
-    {
-        return "numgoods_index";
-    }
-}
+//package edu.ncst.controller;
+//
+//
+//import edu.ncst.service.IngoodsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.List;
+//
+//@Controller
+//@RequestMapping("/ingoods")
+//public class IngoodsController {
+//
+//    @Autowired
+//    private IngoodsService ingoodsService;
+//
+//    @RequestMapping(value = "", method = RequestMethod.GET)
+//    public String index(Model model) {
+//        List<Ingoods> ingoodss =ingoodsService.getAllIngoods();
+//        model.addAttribute("ingoodss",ingoodss);
+//        return "ingoods_index";
+//    }
+//
+//    @RequestMapping(value = "/add", method = RequestMethod.GET)
+//    public String add1(){ return "ingoods_update";}
+//
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    public String add(Ingoods g, Model model){
+//        ingoodsService.add(g);
+//        List<Ingoods> ingoodss = ingoodsService.getAllIngoods();
+//        model.addAttribute("ingoodss",ingoodss);
+//        return "ingoods_index";
+//    }
+//    @RequestMapping(value = "/seek", method = RequestMethod.POST)
+//    public String seek(Integer id, Model model){
+//        Ingoods ingoods =ingoodsService.findone(id);
+//        model.addAttribute("ingoods",ingoods);
+//        return "ingoods_index";
+//    }
+//
+//    @RequestMapping(value = "/update", method = RequestMethod.GET)
+//    public String update1(Integer id,Model model){
+//        Ingoods ingoods =ingoodsService.findone(id);
+//        model.addAttribute("ingoods",ingoods);
+//        return "ingoods_update";
+//    }
+//
+//    @RequestMapping(value = "/update", method = RequestMethod.POST)
+//    public String update(Ingoods g, Model model){
+//        ingoodsService.add(g);
+//        List<Ingoods> ingoodss =ingoodsService.getAllIngoods();
+//        model.addAttribute("ingoodss",ingoodss);
+//        return "ingoods_index";
+//    }
+//
+//    @RequestMapping(value = "/list", method = RequestMethod.GET)
+//    @ResponseBody
+//    public List<Ingoods> list(){
+//        return ingoodsService.getAllIngoods();
+//    }
+//
+//    @RequestMapping(value = "/goods", method = RequestMethod.GET)
+//    public String inGoods()
+//    {
+//        return "goods_index";
+//    }
+//
+//    @RequestMapping(value = "/outgoods", method = RequestMethod.GET)
+//    public String outGoods()
+//    {
+//        return "outgoods_index";
+//    }
+//
+//    @RequestMapping(value = "/numgoods", method = RequestMethod.GET)
+//    public String numGoods()
+//    {
+//        return "numgoods_index";
+//    }
+//}
